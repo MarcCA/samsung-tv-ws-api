575f76e18a065a2b055928646e78e95e9db64a7a
MarcCA <marc_ca_la@live.com>
2024-06-16 22:08:45 -0700

    Update art.py

--- samsungtvws/art.py
+++ samsungtvws/art.py
@@ -505,7 +505,7 @@ class SamsungTVArt(SamsungTVWSConnection):
         NOTE: Not all mattes can be set for all image sizes!
         '''
         art_request = {
-                        "request": "set_matte",
+                        "request": "change_matte",
                         "content_id": content_id,
                         "matte_id": matte_id,
                       }
